37695e38f8430933637f012c78281ce670100de0
Neha Arora <neha.arora@thoughtworks.com>
2018-12-03 15:19:48 +0530

    Added item model for catalog

---
+++ src/main/java/com/wit/catalog/model/Item.java
@@ -0,0 +1,41 @@
+package com.wit.catalog.model;
+
+public class Item {
+    private String name;
+    private int id;
+    private String description;
+    private float price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
+
